a6ba45713ed3e9613df9509a118437bf6463581e
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-06 17:20:15 +0700

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      "cyan-dark": "#2c3333",
+      "gray-very-light": "#ececec",
+      "cyan-blue-medium-ligth": "#f0f1f3",
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("daisyui"),
+  ],
+};

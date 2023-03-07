120cd9c3f813c639d0d7a191fc2f278a71cb3760
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-08 00:20:38 +0700

    Updated Pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,15 +1,38 @@
-export const Pagination = () => {
+import React from "react";
+import { paginationProps } from "@/utils/types/contentsType";
+
+export const Pagination: React.FC<paginationProps> = ({
+  onTotalPages,
+  onPage,
+  onSetPages,
+}) => {
+  const hanlePrevious = () => {
+    onSetPages(onPage - 1);
+  };
+
+  const handleNext = () => {
+    onSetPages(onPage + 1);
+  };
+
   return (
     <>
-      <div className="flex justify-end px-4">
+      <div className="flex justify-end p-4">
         <div className="btn-group">
-          <button className="btn btn-outline text-cyan-dark border-gray-very-light">
+          <button
+            className="btn btn-outline text-cyan-dark border-gray-very-light btn-sm"
+            onClick={hanlePrevious}
+            disabled={onPage === 1}
+          >
             «
           </button>
-          <button className="btn btn-outline text-xs text-cyan-dark border-gray-very-light">
+          <button className="btn btn-outline text-xs text-cyan-dark border-gray-very-light btn-sm">
             Page 1
           </button>
-          <button className="btn btn-outline text-cyan-dark border-gray-very-light">
+          <button
+            className="btn btn-outline text-cyan-dark border-gray-very-light btn-sm"
+            onClick={handleNext}
+            disabled={onPage === onTotalPages}
+          >
             »
           </button>
         </div>

a5d5ff01b774438f4c77c5cec5f3c81c40ab7bff
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-07 17:40:38 +0700

    Updated form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,19 @@
-export const Search = () => {
+import React from "react";
+import { useDebouncedCallback } from "use-debounce";
+import { formProps } from "@/utils/types/contentsType";
+
+export const Search: React.FC<formProps> = ({ onSetQuery }) => {
+  const debounce = useDebouncedCallback((value) => {
+    onSetQuery(value);
+  }, 1000);
+
   return (
     <>
       <input
         type="text"
         placeholder="Search"
         className="input input-bordered w-full h-10 bg-white"
+        onChange={(e) => debounce(e.target.value)}
       />
     </>
   );

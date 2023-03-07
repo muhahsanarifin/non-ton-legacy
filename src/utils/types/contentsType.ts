314d1ce1010db1337f40b95770b33f872e9d1cd6
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-08 00:20:19 +0700

    Updated types

--- src/utils/types/contentsType.ts
+++ src/utils/types/contentsType.ts
@@ -2,7 +2,12 @@ export type contentProps = {
   [key: string]: any;
 };
 
-
 export type formProps = {
   onSetQuery: any;
+};
+
+export type paginationProps = {
+  onTotalPages: number;
+  onSetPages: any;
+  onPage: number;
 };

a153d6d1d2eac30f430dd6cb2ce7d8d9bc5df586
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-07 11:48:18 +0700

    Updated utils

--- src/utils/api/content.ts
+++ src/utils/api/content.ts
@@ -1,10 +1,9 @@
 import Axios from "axios";
-import { contentProps } from "../types/contentsType";
 
 const BASE_URL = process.env.NEXT_PUBLIC_NON_TON_SUPPORT_BY_PULO_DEV;
 
-export const contests = (queryParams: string) => {
+export const getContents = (queryParams: string) => {
   const url = `${BASE_URL}/v1/contents`;
 
-  return Axios.get<Array<contentProps>>(`${url}/${queryParams}`);
+  return Axios.get<any>(`${url}?${queryParams}`);
 };
--- src/utils/types/contentsType.ts
+++ src/utils/types/contentsType.ts
@@ -1,17 +1,3 @@
 export type contentProps = {
-  id: any | null;
-  title: string;
-  url: string;
-  body: string | null;
-  tags: string;
-  media: string;
-  thumbnail: string | null;
-  owner: string;
-  draft: boolean;
-  original_published_at: string;
-  created_at: string;
-  updated_at: string;
-  deleted_at: null;
-  source_id: any | null;
-  contributor: string;
+  [key: string]: any;
 };

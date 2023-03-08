9bf800b302d23369365ec899096eacfb0559ee51
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-08 13:13:33 +0700

    Updated home.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,11 +6,12 @@ import { DateTime } from "luxon";
 
 import { Title } from "@/components/Title";
 import { Header } from "@/components/Header";
-// import { Footer } from "@/components/Footer";
+import { Footer } from "@/components/Footer";
 import { Loader } from "@/components/Loader";
 import { Search } from "@/components/Form";
 import { Pagination } from "@/components/Pagination";
 import { ResetButton } from "@/components/Button";
+import { Modal } from "@/components/Modal";
 
 const Home = () => {
   const [contents, setContents] = useState<contentProps>([]);
@@ -72,53 +73,56 @@ const Home = () => {
     return DateTime.fromISO(date).toFormat("ff");
   };
 
+  const mediaArrOfObjs = [
+    {
+      id: 0,
+      media: "web",
+      page: 1,
+    },
+    {
+      id: 1,
+      media: "tulisan",
+      page: 1,
+    },
+    {
+      id: 2,
+      media: "podcast",
+      page: 1,
+    },
+    {
+      id: 3,
+      media: "video",
+      page: 1,
+    },
+  ];
+
+  // console.log("Result:", mediaArrOfObjs);
+
   return (
     <>
       <Title name="Home" />
+      <Modal onIdModal="about-modal" />
       <div className="flex flex-col">
         <Header />
         <main className="bg-white flex flex-row pt-16">
-          <section className="w-52 shadow">
-            <div className="relative">
+          <section className="w-52 shadow xs:hidden">
+            <aside className="relative">
               <div className="fixed p-2 w-52 min-h-screen">
                 <ul className="menu text-cyan-dark">
                   <li className="menu-title my-2 shadow p-2">Media</li>
-                  <li
-                    className="text-sm cursor-pointer my-1 hover:bg-cyan-blue-medium-ligth p-2 rounded-full"
-                    onClick={() => {
-                      setMedia("web");
-                      setPage(1);
-                    }}
-                  >
-                    Web
-                  </li>
-                  <li
-                    className="text-sm cursor-pointer my-1 hover:bg-cyan-blue-medium-ligth p-2 rounded-full"
-                    onClick={() => {
-                      setMedia("tulisan");
-                      setPage(1);
-                    }}
-                  >
-                    Tulisan
-                  </li>
-                  <li
-                    className="text-sm cursor-pointer my-1 hover:bg-cyan-blue-medium-ligth p-2 rounded-full"
-                    onClick={() => {
-                      setMedia("podcast");
-                      setPage(1);
-                    }}
-                  >
-                    Podcast
-                  </li>
-                  <li
-                    className="text-sm cursor-pointer my-1 hover:bg-cyan-blue-medium-ligth p-2 rounded-full"
-                    onClick={() => {
-                      setMedia("video");
-                      setPage(1);
-                    }}
-                  >
-                    Video
-                  </li>
+                  {mediaArrOfObjs?.map((mediaArrOfObj) => (
+                    <>
+                      <li
+                        className="text-sm cursor-pointer my-1 hover:bg-cyan-blue-medium-ligth p-2 rounded-full capitalize font-semibold"
+                        onClick={() => {
+                          setMedia(mediaArrOfObj.media);
+                          setPage(mediaArrOfObj.page);
+                        }}
+                      >
+                        {mediaArrOfObj.media}
+                      </li>
+                    </>
+                  ))}
                 </ul>
                 <div className="absolute bottom-24">
                   <Pagination
@@ -131,14 +135,14 @@ const Home = () => {
                   />
                 </div>
               </div>
-            </div>
+            </aside>
           </section>
 
-          <section className=" p-4 flex-1 flex flex-col gap-y-4">
+          <section className=" p-4 flex-1 flex flex-col gap-y-4 xs:w-full">
             <div>
               <Search onSetQuery={setQuery} />
             </div>
-            <div className="rounded-md shadow min-h-screen">
+            <div className="rounded-md shadow min-h-screen ">
               {media || query || page > 1 ? (
                 <div className="flex justify-end px-4">
                   <ResetButton />
@@ -186,7 +190,7 @@ const Home = () => {
             </div>
           </section>
         </main>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </>
   );

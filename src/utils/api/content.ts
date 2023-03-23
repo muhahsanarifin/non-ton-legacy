67741ede907337ad00af671cde356d41d34f6d0e
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-23 14:34:04 +0700

    Updated content.ts

--- src/utils/api/content.ts
+++ src/utils/api/content.ts
@@ -3,7 +3,7 @@ import Axios from "axios";
 const BASE_URL = process.env.NEXT_PUBLIC_NON_TON_SUPPORT_BY_PULO_DEV;
 
 export const getContents = (queryParams: string) => {
-  const url = `${BASE_URL}/v1/contents`;
+  const url = `${BASE_URL}/contents`;
 
   return Axios.get<any>(`${url}?${queryParams}`);
 };

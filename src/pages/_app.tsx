f42b92abe4fe246412020e1a1dc073e354694342
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-20 12:55:52 +0700

    Updated _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import store, { persistedStore } from "../redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
